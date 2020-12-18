b59f363ffee09add11efd7a01e9965edf8854be4
Chris <kuch517@gmail.com>
2020-12-18 09:08:51 -0800

    Created uperprofile app and started user creation

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.urls import include, path
 from rest_framework import routers
-from quickstart import views
+from userprofile import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -24,7 +24,6 @@ from rest_framework_simplejwt.views import (
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
