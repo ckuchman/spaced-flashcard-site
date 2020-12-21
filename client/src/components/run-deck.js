22d71c14eb634ac73ade67db08da7bd4b43c5723
contip <contip@oregonstate.edu>
2020-12-20 20:19:31 -0500

    last minute changes

--- client/src/components/run-deck.js
+++ client/src/components/run-deck.js
@@ -1,28 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import CardDisplay from "./card-display";
 import DeckInfo from "./deck-info";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
+import { fetchCall } from "./helpers";
 
-const sampleDeck = [
-  { question: "2+2=", answer: "4" },
-  { question: "2+3=", answer: "5" },
-  { question: "2+4=", answer: "6" },
-  { question: "2+5=", answer: "7" },
-  { question: "2+6=", answer: "8" },
-  { question: "2+7=", answer: "9" },
-  { question: "2+8=", answer: "10" },
-];
+// const sampleDeck = [
+//   { question: "2+2=", answer: "4" },
+//   { question: "2+3=", answer: "5" },
+//   { question: "2+4=", answer: "6" },
+//   { question: "2+5=", answer: "7" },
+//   { question: "2+6=", answer: "8" },
+//   { question: "2+7=", answer: "9" },
+//   { question: "2+8=", answer: "10" },
+// ];
 
 /* props should contain either the deck card data itself or the deck_id */
 export default function RunDeck() {
-  let history = useHistory();
+  const location = useLocation();
+  const history = useHistory();
   const [cardIndex, setCardIndex] = useState(0);
+  const user_deck_id = location.state.user_deck_id;
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let payload = {
+        url:
+          process.env.REACT_APP_BASE_URL +
+          `api/userdecks/card_list/?userdeck=${user_deck_id}`,
+        method: "GET",
+        auth: true,
+      };
+      let response = await fetchCall(payload);
+      console.log(
+        `returned list of cards for deck with user_deck_id=${user_deck_id} is: ${JSON.stringify(
+          response
+        )}`
+      );
+      setCards(response);
+    }
+    fetchData();
+  }, []);
 
   /* increases cardIndex to display the next card in deck */
   function nextCard() {
     console.log(`i'm calling nextcard`);
-    if (cardIndex !== sampleDeck.length - 1) {
+    if (cardIndex !== cards.length - 1) {
       setCardIndex(cardIndex + 1);
     } else {
       setCardIndex(-1);
@@ -30,20 +54,23 @@ export default function RunDeck() {
   }
 
   function redirectHome() {
-    history.push("/profile");
+    history.push({
+      pathname: "/profile",
+      state: { user_id: location.state.user_id },
+    });
     return;
   }
 
   /* need to pass CardDisplay userid, deckid, etc.. */
   return (
     <>
-      {cardIndex !== -1 ? (
+      {cards.length > 0 && cardIndex !== -1 ? (
         <>
-          <DeckInfo numCards={sampleDeck.length} cardIndex={cardIndex} />
+          <DeckInfo numCards={cards.length} cardIndex={cardIndex} />
           <CardDisplay
             index={cardIndex}
-            question={sampleDeck[cardIndex].question}
-            answer={sampleDeck[cardIndex].answer}
+            question={cards[cardIndex].question}
+            answer={cards[cardIndex].answer}
             nextCard={nextCard}
           />
         </>
